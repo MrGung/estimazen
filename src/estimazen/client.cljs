14c35e7b4167f0a7db8886035531b317cd61c60c
Steffen Glückselig <gist@gungfu.de>
2020-11-17 06:18:59 +0100

    Code for Login (currently necessary!?) -> necessary for identifying users on the server?

--- src/estimazen/client.cljs
+++ src/estimazen/client.cljs
@@ -106,6 +106,34 @@
         (->output! "est-Button was clicked: %s" (.-textContent target-el))
         (chsk-send! [:estimazen/est-button {:btn-value (.-textContent target-el) :had-a-callback? "nope"}])))))
 
+(when-let [target-el (.getElementById js/document "btn-login")]
+  (.addEventListener target-el "click"
+    (fn [ev]
+      (let [user-id (.-value (.getElementById js/document "input-login"))]
+        (if (str/blank? user-id)
+          (js/alert "Please enter a user-id first")
+          (do
+            (->output! "Logging in with user-id %s" user-id)
+
+            ;;; Use any login procedure you'd like. Here we'll trigger an Ajax
+            ;;; POST request that resets our server-side session. Then we ask
+            ;;; our channel socket to reconnect, thereby picking up the new
+            ;;; session.
+
+            (sente/ajax-lite "/login"
+              {:method :post
+               :headers {:X-CSRF-Token (:csrf-token @chsk-state)}
+               :params  {:user-id (str user-id)}}
+
+              (fn [ajax-resp]
+                (->output! "Ajax login response: %s" ajax-resp)
+                (let [login-successful? true] ; Your logic here
+
+                  (if-not login-successful?
+                    (->output! "Login failed")
+                    (do
+                      (->output! "Login successful")
+                      (sente/chsk-reconnect! chsk))))))))))))
 
 ;;;; Init stuff
 
