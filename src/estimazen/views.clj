dd0ff8cb8229ec40d18dfb18f1ec75bda04dc09f
Steffen Glückselig <gist@gungfu.de>
2020-11-20 06:31:31 +0100

    a little bit of layout - a little bit of style

--- src/estimazen/views.clj
+++ src/estimazen/views.clj
@@ -16,18 +16,16 @@
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]]
      [:body
       [:div {:class "bg"}
-       [:h1 "estimazen"]
        [:div#est-results]
+       [:h1 "estimazen"]
        (let [csrf-token #_(:anti-forgery-token ring-req) (force anti-forgery/*anti-forgery-token*)]
          [:div#sente-csrf-token {:data-csrf-token csrf-token}])
-       #_[:p
-          [:input#input-login {:type :text :placeholder "User-id"}]
-          [:button#btn-login {:type "button"} "login!"]]
        [:p
         (for [btn-id [1 2 3 5 8 13]]
           [:button {:id (str "est-btn" btn-id), :type "button", :class "est-btn"} btn-id])
-        [:button {:id (str "est-btn-abstent" ), :type "button", :class "est-btn"} "Enthaltung"]]
+        [:button {:id (str "est-btn-abstent"), :type "button", :class "est-btn"} "Enthaltung"]]
        [:p
-        [:button {:id "est-btn-ready", :type "button"} "Bereit für nächste Schätzung"]
+         [:button {:id "est-btn-ready", :type "button"} "Bereit für nächste Schätzung"]]
+       [:p
         [:textarea {:id "output", :style "width: 100%; height: 200px;"}]]
        [:script {:src "main.js"}]]]]))
