ee8fbc422cc751488e29ef5cdaa1e442ca495e6a
Steffen Glückselig <gist@gungfu.de>
2020-11-27 06:19:22 +0100

    Layout: results shown at bottom.

--- src/estimazen/views.clj
+++ src/estimazen/views.clj
@@ -16,9 +16,7 @@
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]]
      [:body
       [:div {:class "bg"}
-       [:div#estimations
-        [:p#est-stats "Schätzungen " [:span#est-stats-estimated "0"] "/" [:span#est-stats-clients "0"]]
-        [:div#est-results]]
+
        [:h1 "estimazen"]
        (let [csrf-token #_(:anti-forgery-token ring-req) (force anti-forgery/*anti-forgery-token*)]
          [:div#sente-csrf-token {:data-csrf-token csrf-token}])
@@ -26,6 +24,9 @@
         (for [btn-id [1 2 3 5 8 13]]
           [:button {:id (str "est-btn" btn-id), :type "button", :class "est-btn"} btn-id])
         [:button {:id (str "est-btn-abstent"), :type "button", :class "est-btn"} "Enthaltung"]]
+       [:div#estimations
+        [:p#est-stats "Schätzungen " [:span#est-stats-estimated "0"] "/" [:span#est-stats-clients "0"]]
+        [:div#est-results]]
        [:p
         [:textarea {:id "output", :style "width: 100%; height: 200px;"}]]
        [:script {:src "main.js"}]]]]))
