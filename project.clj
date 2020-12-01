af271f059961c61d4405ae8188f398c0fa26d7a3
Steffen Glückselig <gist@gungfu.de>
2020-12-01 05:56:56 +0100

    uberjar: dev and uberjar profiles

--- project.clj
+++ project.clj
@@ -26,40 +26,44 @@
    [compojure "1.6.2"]                                      ; Or routing lib of your choice
    [hiccup "1.0.5"]]                                        ; Optional, just for HTML
 
-
   :plugins
-  [[lein-pprint "1.3.2"]                                    ;; pprinting project map
-   [lein-ancient "0.6.15"]                                  ;; A Leiningen plugin to check your project for outdated dependencies and plugins.
-   ;[com.cemerick/austin "0.1.6"] ;; cljs REPL
-   [lein-cljsbuild "1.1.8"]                                 ;; compile ClojureScript into Javascript whenever modified
-   [lein-figwheel "0.5.20"]]
+  [;[com.cemerick/austin "0.1.6"] ;; cljs REPL
+   [lein-cljsbuild "1.1.8"]]                                ;; compile ClojureScript into Javascript whenever modified
 
-
-
-  :cljsbuild {:builds
-              [{:id :cljs-client
-                :source-paths ["src"]
-                :figwheel true
-                :jar true
-                :compiler {:main estimazen.client
-                           :asset-path "js/out"             ;https://clojurescript.org/reference/compiler-options#asset-path
-                           :output-to "resources/public/main.js"
-                           :output-dir "resources/public/js/out"
-                           :optimizations :none             ;:whitespace #_:advanced
-                           :source-map-timestamp true
-                           #_#_:source-map true
-                           :pretty-print true}}]}
   :main estimazen.server
 
-  :hooks [leiningen.cljsbuild]
-  :profiles {:uberjar {:aot :all}}
+  :cljsbuild {:builds
+              [{:source-paths ["src"]}]
+              :compiler {
+                         :main estimazen.client
+                         :asset-path "js/out"               ;https://clojurescript.org/reference/compiler-options#asset-path
+                         :output-to "resources/public/main.js"
+                         :output-dir "resources/public/js/out"
+                         :optimizations :advanced}}
+
+  :profiles {:dev {:plugins [[lein-pprint "1.3.2"]          ;; pprinting project map
+                             [lein-ancient "0.6.15"]        ;; A Leiningen plugin to check your project for outdated dependencies and plugins.
+                             [lein-figwheel "0.5.20"]]
+                   :figwheel {:css-dirs ["resources/public/css"]}
+                   :cljsbuild {:builds
+                               [{
+                                 :figwheel true
+                                 :jar true
+                                 :compiler {:optimizations :none ;:whitespace #_:advanced
+                                            :source-map-timestamp true
+                                            :source-map true
+                                            :pretty-print true}}]}}
+             :uberjar {:hooks [leiningen.cljsbuild]
+                       :aot :all
+                       :cljsbuild {:builds
+                                   [{:jar true
+                                     :compiler {:optimizations :advanced}}]}}}
 
   ;; files removed by `lein clean`
   :clean-targets ^{:protect false} ["resources/public/main.js" "target" "resources/public/js/out"]
 
-
   :source-paths ["src"]
-  :figwheel {:css-dirs ["resources/public/css"]}
+
 
   ;; Call `lein start-repl` to get a (headless) development repl that you can
   ;; connect to with Cider+emacs or your IDE of choice:
