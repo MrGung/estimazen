1d33cf6866216c5956f5190be56bd916dc5b7772
Steffen Glückselig <gist@gungfu.de>
2021-01-06 12:03:25 +0100

    Hinweise zur Nutzung + Style

--- src/estimazen/views.clj
+++ src/estimazen/views.clj
@@ -5,6 +5,20 @@
 
 
 
+(defn hinweise-zur-verwendung []
+  [:ul#hinweise
+   [:li "Funktioniert nur in der VDI."]
+   [:li "Funktioniert nur zuverlässig im (neuen) Edge (der mit der Welle) und Chrome."]
+   [:li "URL ändert sich bei jedem Mal (zumindest ist davon auszugehen). Bookmarken daher sinnlos."]
+   [:li "Solange die Schätzungen in der List noch nicht angezeigt werden, kann man seine Schätzung durch Klick auf einen anderen Wert ändern. Auch Enthaltung ist möglich."]
+   [:li "Die Buttonfarbe hat folgende Bedeutung:"
+    [:ul
+     [:li "Durchscheinendes Weiß => noch keine eigene Schätzung abgegeben."]
+     [:li "Deckendes Weiß => die aktuell aktive eigene Schätzung."]
+     [:li "Dunkelgrau => Ergebnisse wurden angezeigt, die Schätzung wurde berücksichtigt."]]]
+   [:li "Wenn ein Drittel der Schätzer einen Wert abgegeben hat, wird die Ergebnisliste geleert, um so die nächste Runde zu signalisieren."]])
+
+
 
 (defn estimazen-landing-pg-handler [ring-req]
   (page/html5
@@ -16,8 +30,8 @@
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]]
      [:body
       [:div {:class "bg"}
-
        [:h1 "estimazen"]
+       [:div (hinweise-zur-verwendung)]
        (let [csrf-token #_(:anti-forgery-token ring-req) (force anti-forgery/*anti-forgery-token*)]
          [:div#sente-csrf-token {:data-csrf-token csrf-token}])
        [:p
@@ -29,4 +43,7 @@
         [:div#est-results]]
        [:p
         [:textarea {:id "output", :style "width: 100%; height: 200px;"}]]
+
        [:script {:src "main.js"}]]]]))
+
+
