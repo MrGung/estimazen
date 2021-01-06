dc9074e2c7bdef7a4504dd4240994fe81c1e412e
Steffen Glückselig <gist@gungfu.de>
2021-01-06 12:27:58 +0100

    Hinweise zur Nutzung: weiter gestylt.

--- src/estimazen/views.clj
+++ src/estimazen/views.clj
@@ -6,17 +6,17 @@
 
 
 (defn hinweise-zur-verwendung []
-  [:ul#hinweise
-   [:li "Funktioniert nur in der VDI."]
-   [:li "Funktioniert nur zuverlässig im (neuen) Edge (der mit der Welle) und Chrome."]
-   [:li "URL ändert sich bei jedem Mal (zumindest ist davon auszugehen). Bookmarken daher sinnlos."]
-   [:li "Solange die Schätzungen in der List noch nicht angezeigt werden, kann man seine Schätzung durch Klick auf einen anderen Wert ändern. Auch Enthaltung ist möglich."]
-   [:li "Die Buttonfarbe hat folgende Bedeutung:"
-    [:ul
-     [:li "Durchscheinendes Weiß => noch keine eigene Schätzung abgegeben."]
-     [:li "Deckendes Weiß => die aktuell aktive eigene Schätzung."]
-     [:li "Dunkelgrau => Ergebnisse wurden angezeigt, die Schätzung wurde berücksichtigt."]]]
-   [:li "Wenn ein Drittel der Schätzer einen Wert abgegeben hat, wird die Ergebnisliste geleert, um so die nächste Runde zu signalisieren."]])
+  [:ul
+   [:li "Funktioniert nur in der " [:em "VDI"] "."]
+   [:li "Funktioniert nur zuverlässig im (neuen) " [:em "Edge"] " (der mit der Welle) und " [:em "Chrome"] "."]
+   [:li [:em "URL ändert sich"] " bei jedem Mal (zumindest ist davon auszugehen). Bookmarken daher sinnlos."]
+   [:li "Solange die Schätzungen in der List noch nicht angezeigt werden, kann man seine " [:em "Schätzung durch Klick auf einen anderen Wert ändern"] ". Auch Enthaltung ist möglich."]
+   [:li "Die " [:em "Buttonfarbe"] " hat folgende Bedeutung:"
+    [:dl
+     [:dt "Durchscheinendes Weiß"] [:dd "noch keine eigene Schätzung abgegeben."]
+     [:dt "Deckendes Weiß"] [:dd "die aktuell aktive eigene Schätzung."]
+     [:dt "Dunkelgrau"] [:dd "Ergebnisse wurden angezeigt, die Schätzung wurde berücksichtigt."]]]
+   [:li "Wenn ein Drittel der Schätzer einen Wert abgegeben hat, wird die " [:em "Ergebnisliste geleert"] ", um so die nächste Runde zu signalisieren."]])
 
 
 
@@ -31,7 +31,9 @@
      [:body
       [:div {:class "bg"}
        [:h1 "estimazen"]
-       [:div (hinweise-zur-verwendung)]
+       [:div#hinweise
+        [:h2 "Hinweise zur Nutzung"]
+        (hinweise-zur-verwendung)]
        (let [csrf-token #_(:anti-forgery-token ring-req) (force anti-forgery/*anti-forgery-token*)]
          [:div#sente-csrf-token {:data-csrf-token csrf-token}])
        [:p
